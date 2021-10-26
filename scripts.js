3cc96943f666c3459b97d614237c870d853dfa07
caio lucena <caio.lcolaco@gmail.com>
2021-10-25 22:32:47 -0300

    the modal content is actually set

--- scripts.js
+++ scripts.js
@@ -4,10 +4,11 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
     card.addEventListener("click", function(){
         const imgId = card.getAttribute("id")
-        const title = card.getElementsByTagName('h1')
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('img').src = `/bootcamp-launchbase-desafios-02-master/layouts/assets/${imgId}.png`
-        modalOverlay.querySelector('h1') = title
+
+        modalOverlay.querySelector("h1").innerHTML = card.querySelector("h2").textContent
+        modalOverlay.querySelector("h2").innerHTML = card.querySelector("h3").textContent
     })
 }
 /* precisando ajeitar o preenchimento dos titulos do modal */
