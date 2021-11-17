36af01bce56a0fb8e7269dc26901dee2a1a3cb14
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-11-17 19:13:21 -0300

    criando rotas, esta tudo querbrado

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get("/", function(req, res) {
-    return res.render('home.html')
+    return res.render('home')
+})
+
+server.get("/sobre", function(req, res) {
+    return res.render('sobre')
+})
+
+server.get("/receitas", function(req, res) {
+    return res.render('receitas')
 })
 
 server.listen(5000, function() {
