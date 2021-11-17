74828e071b8f375805d3bf8d76c47815b75d813f
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-11-17 18:43:57 -0300

    added server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
+server.get("/", function(req, res) {
+    return res.render('home.html')
+})
+
 server.listen(5000, function() {
     console.log("server is running")
 })
