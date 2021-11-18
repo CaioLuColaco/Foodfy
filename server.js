9b095115f7f693f37aae2114cdcda7ebf5635460
caio lucena <caio.lcolaco@gmail.com>
2021-11-18 09:58:42 -0300

    install and configurated the nunjucks

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.set("view engine", 'html')
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express:server
+})
 
 server.get("/", function(req, res) {
     return res.render('home')
